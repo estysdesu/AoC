0673c036ae4b24f94705e42b4083a1ee7c1143ea
Tyler Estes <estysdesu@gmail.com>
2019-12-20 12:27:17 -0500

    2019/02 part1

--- 2019/02/src/main.rs
+++ 2019/02/src/main.rs
@@ -1,3 +1,41 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "./input.txt";
+    let mut intcodes: Vec<u32> = process_input(filename);
+
+    // restore to state before crash
+    intcodes[1] = 12; // modify pos 1 to value of 12
+    intcodes[2] = 2; // modify pos 2 to value of 2
+
+    intcode_processor(&mut intcodes);
+    println!("{:?}", intcodes);
+}
+
+fn process_input(filename: &str) -> Vec<u32> {
+    let contents = fs::read_to_string(filename).expect("error reading file.");
+    let content_vec: Vec<u32> = contents
+        .split(",")
+        .map(|m| m.parse().expect("error parsing str to int"))
+        .collect();
+    return content_vec;
+}
+
+fn intcode_processor(intcodes: &mut Vec<u32>) {
+    const OPCODE_ADD: u32 = 1;
+    const OPCODE_MULTIPLY: u32 = 2;
+    const OPCODE_EXIT: u32 = 99;
+
+    for i in 0..(intcodes.len() / 4) {
+        let opcode = intcodes[i * 4];
+        let n1_i = intcodes[(i * 4) + 1] as usize;
+        let n2_i = intcodes[(i * 4) + 2] as usize;
+        let output_i = intcodes[(i * 4) + 3] as usize;
+        match opcode {
+            OPCODE_ADD => intcodes[output_i] = intcodes[n1_i] + intcodes[n2_i],
+            OPCODE_MULTIPLY => intcodes[output_i] = intcodes[n1_i] * intcodes[n2_i],
+            OPCODE_EXIT => break,
+            _ => panic!("invalid intcode sequence"),
+        };
+    }
 }
