8d133c33f460635bf2a445bd4ca351e80aa2592d
Tyler Estes <estysdesu@gmail.com>
2019-12-20 12:28:08 -0500

    day 1

--- 2019/01/src/main.rs
+++ 2019/01/src/main.rs
@@ -2,12 +2,12 @@ use std::fs;
 
 fn main() {
     let filename = "./input.txt";
-    let contents = fs::read_to_string(&filename).expect("Error reading file.");
+    let contents = fs::read_to_string(&filename).expect("error reading file.");
     let lines = contents.lines();
 
     let mut total_fuel: u32 = 0;
     for line in lines {
-        let mass: u32 = line.parse().expect("Error parsing str -> int");
+        let mass: u32 = line.parse().expect("error parsing str -> int");
 
         let module_fuel = get_module_fuel(mass);
         total_fuel += module_fuel;
