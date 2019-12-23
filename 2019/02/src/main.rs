64a5fa2574da46613f7c9b2f91a78766c8c92b06
Tyler Estes <estysdesu@gmail.com>
2019-12-22 21:44:48 -0500

    complete day 02

--- 2019/02/src/main.rs
+++ 2019/02/src/main.rs
@@ -3,10 +3,21 @@ extern crate intcode;
 
 fn main() {
     let filename = "./input.txt";
-    let mut intcodes: Vec<u32> = process_input(filename);
+    let orig_intcodes: Vec<u32> = process_input(filename);
 
-    intcode::process(&mut intcodes, Some(12), Some(2));
-    println!("{:?}", intcodes);
+    for n in 0..=99 {
+        // noun
+        for v in 0..=99 {
+            // verb
+            let mut intcodes = orig_intcodes.clone();
+            intcode::process(&mut intcodes, Some(n), Some(v));
+            let output = intcodes[0];
+            if output == 19690720 {
+                println!("{}", 100 * n + v)
+            }
+        }
+    }
+    // println!("{:?}", intcodes);
 }
 
 fn process_input(filename: &str) -> Vec<u32> {
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AoC
 [Advent of Code](https://adventofcode.com) repo to be continued every year.
+
+##[2019/01]()
+
+##[2019/02]()
